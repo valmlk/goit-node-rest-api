19352e70af57f6bf577cac42c254a9903efaf8db
valmlk <valyushka.malko@gmail.com>
2024-04-28 18:13:10 +0200

    add user avatar

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,6 +1,10 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import gravatar from 'gravatar';
+import path from 'path';
+import { promises as fs } from 'fs';
+import Jimp from 'jimp';
 import HttpError from '../helpers/HttpError.js';
 
 import User from '../models/user.js';
@@ -8,6 +12,8 @@ import User from '../models/user.js';
 dotenv.config();
 
 const { SECRET_KEY } = process.env;
+
+const avatarsDir = path.join(process.cwd(), 'public', 'avatars');
 
 export const registerUser = async (req, res, next) => {
   try {
@@ -19,8 +25,9 @@ export const registerUser = async (req, res, next) => {
     }
 
     const hashPassword = await bcrypt.hash(password, 10);
+    const avatarURL = gravatar.url(email);
 
-    const newUser = await User.create({ ...req.body, password: hashPassword });
+    const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL });
 
     res.status(201).json({
       user: {
@@ -51,7 +58,7 @@ export const loginUser = async (req, res, next) => {
     };
 
     const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '6h' });
-    await User.findByIdAndUpdate(user._id, {token: token});
+    await User.findByIdAndUpdate(user._id, { token: token });
 
     res.status(200).json({
       token,
@@ -84,6 +91,32 @@ export const logOutUser = async (req, res, next) => {
     await User.findByIdAndUpdate(_id, { token: '' });
 
     res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateAvatar = async (req, res, next) => {
+  try {
+    const { _id } = req.user;
+    const { path: tempUpload, originalname } = req.file;
+    const filename = `${_id}_${originalname}`;
+    const resultUpload = path.join(avatarsDir, filename);
+
+    await Jimp.read(tempUpload)
+      .then(image => {
+        return image.resize(250, 250).write(tempUpload);
+      })
+      .catch(error => {
+        throw error;
+      });
+
+    await fs.rename(tempUpload, resultUpload);
+    const avatarURL = path.join('avatars', filename);
+
+    await User.findByIdAndUpdate(_id, { avatarURL });
+
+    res.status(200).json({ avatarURL });
   } catch (error) {
     next(error);
   }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,9 +4,11 @@ import {
   loginUser,
   getCurrentUser,
   logOutUser,
+  updateAvatar,
 } from '../controllers/usersControllers.js';
 import { checkUserData } from '../helpers/checkUser.js';
 import { registerSchema, loginSchema } from '../schemas/userSchema.js';
+import { upload } from '../helpers/upload.js';
 import validateBody from '../helpers/validateBody.js';
 
 const userRouter = express.Router();
@@ -18,5 +20,7 @@ userRouter.post('/login', validateBody(loginSchema), loginUser);
 userRouter.post('/logout', checkUserData, logOutUser);
 
 userRouter.get('/current', checkUserData, getCurrentUser);
+
+userRouter.patch('/avatars', checkUserData, upload.single('avatar'), updateAvatar);
 
 export default userRouter;
